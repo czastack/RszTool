8d9e2f53c93a5c8223e2252457ed6d965702aba5
czastack <2464851375@qq.com>
2023-07-07 00:38:07 +0800

    dev rsz

--- RszTool/Models.cs
+++ RszTool/Models.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace RszTool
 {
@@ -13,21 +14,28 @@ namespace RszTool
             return Fields.FirstOrDefault(f => f.Name == name);
         }
 
-        public void AddField<T>(string name, int size = 0, int offset = 0)
+        public DataField AddField<T>(string name, int size = 0, int offset = 0, int align = 0)
         {
             var type = typeof(T);
-            if (type.IsValueType)
+            if (type.IsValueType && size == 0)
             {
-                if (size == 0) {
-                    size = Unsafe.SizeOf<T>();
-                }
+                size = Unsafe.SizeOf<T>();
             }
             if (offset == 0)
             {
                 offset = Size;
-                Size += size;
+                if (align != 0)
+                {
+                    offset = Utils.AlignSize(offset, align);
+                }
             }
-            Fields.Add(new DataField(name, size, offset, type));
+            if (offset + size >= Size)
+            {
+                Size = offset + size;
+            }
+            var field = new DataField(name, size, offset, type);
+            Fields.Add(field);
+            return field;
         }
     }
 
@@ -44,11 +52,14 @@ namespace RszTool
         public long Start { get; set; }
         public DataClass Class { get; set; }
         public List<DataField> Fields => Class.Fields;
+        public WeakReference<RszFileHandler>? HandlerRef { get; set; }
 
-        public void AddField<T>(string name, T value, int size = 0, int offset = 0)
+        public DataField AddField<T>(string name, T value, int size = 0, int offset = 0, int align = 0)
         {
-            Class.AddField<T>(name, size, offset);
+            return Class.AddField<T>(name, size, offset);
         }
+
+        public RszFileHandler? Handler => HandlerRef?.GetTarget();
     }
 
     public class DataField
@@ -64,7 +75,122 @@ namespace RszTool
         public string Name { get; set; } = "";
         public int Size { get; set; }
         public int Offset { get; set; }
-        public int Align { get; set; }
         public Type Type { get; set; }
+
+        public long GetAddress(DataObject instance)
+        {
+            return instance.Start + Offset;
+        }
+
+        public virtual object? ReadValue(DataObject instance)
+        {
+            var handler = instance.Handler;
+            if (handler != null)
+            {
+                handler.FSeek(GetAddress(instance));
+                Type dataType = Type;
+                BinaryReader reader = handler.Reader;
+                if (dataType == typeof(int))
+                    return reader.ReadInt32();
+                else if (dataType == typeof(uint))
+                    return reader.ReadUInt32();
+                else if (dataType == typeof(byte))
+                    return reader.ReadByte();
+                else if (dataType == typeof(sbyte))
+                    return reader.ReadSByte();
+                else if (dataType == typeof(short))
+                    return reader.ReadInt16();
+                else if (dataType == typeof(ushort))
+                    return reader.ReadUInt16();
+                else if (dataType == typeof(long))
+                    return reader.ReadInt64();
+                else if (dataType == typeof(ulong))
+                    return reader.ReadUInt64();
+                else if (dataType == typeof(float))
+                    return reader.ReadSingle();
+                else if (dataType == typeof(double))
+                    return reader.ReadDouble();
+                else if (dataType == typeof(bool))
+                    return reader.ReadBoolean();
+                // else if (dataType == typeof(string))
+                //     return reader.ReadString();
+            }
+            return null;
+        }
+
+        public virtual bool WriteValue(DataObject instance, object value)
+        {
+            var handler = instance.Handler;
+            if (handler != null)
+            {
+                handler.FSeek(GetAddress(instance));
+                Type dataType = Type;
+                BinaryWriter writer = handler.Writer;
+                if (dataType == typeof(int))
+                    writer.Write(Convert.ToInt32(value));
+                else if (dataType == typeof(uint))
+                    writer.Write(Convert.ToUInt32(value));
+                else if (dataType == typeof(byte))
+                    writer.Write(Convert.ToByte(value));
+                else if (dataType == typeof(sbyte))
+                    writer.Write(Convert.ToSByte(value));
+                else if (dataType == typeof(short))
+                    writer.Write(Convert.ToInt16(value));
+                else if (dataType == typeof(ushort))
+                    writer.Write(Convert.ToUInt16(value));
+                else if (dataType == typeof(long))
+                    writer.Write(Convert.ToInt64(value));
+                else if (dataType == typeof(ulong))
+                    writer.Write(Convert.ToUInt64(value));
+                else if (dataType == typeof(float))
+                    writer.Write(Convert.ToSingle(value));
+                else if (dataType == typeof(double))
+                    writer.Write(Convert.ToDouble(value));
+                else if (dataType == typeof(bool))
+                    writer.Write(Convert.ToBoolean(value));
+                // else if (dataType == typeof(string))
+                //     writer.Write(value.ToString()!);
+                else
+                    return false;
+                return true;
+            }
+            return false;
+        }
     }
+
+
+    /*
+    public class DataField<T> : DataField where T : struct
+    {
+        public DataField(string name, int size, int offset) : base(name, size, offset, typeof(T))
+        {
+        }
+
+        public override object? ReadValue(DataObject instance)
+        {
+            var handler = instance.Handler;
+            if (handler != null)
+            {
+                Span<byte> buffer = stackalloc byte[Size];
+                handler.FSeek(GetAddress(instance));
+                handler.Reader.Read(buffer);
+                return Utils.BytesAsStructure<T>(buffer);
+            }
+            return null;
+        }
+
+        public override bool WriteValue(DataObject instance, object value)
+        {
+            var handler = instance.Handler;
+            if (handler != null)
+            {
+                T data = (T)Convert.ChangeType(value, typeof(T))!;
+                Span<byte> buffer = Utils.StructureAsBytes(ref data);
+                handler.FSeek(GetAddress(instance));
+                handler.Writer.Write(buffer);
+                return true;
+            }
+            return false;
+        }
+    } */
 }
--- RszTool/Utils.cs
+++ RszTool/Utils.cs
@@ -1,3 +1,6 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 namespace RszTool
 {
     public struct TimerRecord
@@ -19,6 +22,63 @@ namespace RszTool
         {
             long endUs = DateTime.Now.Ticks / 10;
             Console.WriteLine($"time of {Name}: {endUs - StartUs} us");
+        }
+    }
+
+
+    public static class Utils
+    {
+        /// <summary>
+        /// 对齐字节
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static int AlignSize(int n, int size) => (n + (size - 1)) & ~(size - 1);
+
+        /// <summary>
+        /// 对齐字节
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static long AlignSize(long n, int size)
+        {
+            var tail = n & (size - 1);
+            if (tail != 0)
+                n += size - tail;
+            return n;
+        }
+
+        /// <summary>
+        /// 对齐字节
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static IntPtr AlignSize(IntPtr n, int size) => (IntPtr)AlignSize((long)n, size);
+
+        /// <summary>
+        /// 对齐4字节
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Align4(int n) => AlignSize(n, 4);
+
+        /// <summary>
+        /// 对齐8字节
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Align8(int n) => AlignSize(n, 8);
+    }
+
+    public static class Extensions
+    {
+        public static T? GetTarget<T>(this WeakReference<T> reference) where T : class
+        {
+            reference.TryGetTarget(out T? target);
+            return target;
         }
     }
 }
